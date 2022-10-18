71beac224b700283051f0a45da9fdcb0d38a40c3
cinthia <evelyng23cr@gmail.com>
2022-10-18 10:28:32 -0500

    inicio del curso de javascript

---
+++ 2.variables/index.js
@@ -0,0 +1,27 @@
+//variables
+// una variable es un espacio donde almacenos 
+//un tipo de dato
+//para poder reutilizarlo
+
+//uso de una variable
+//palabra reservada es una instruccion propia
+//del lenguaje de programacion
+
+//palabra reservada para declarar una variable
+//existen 2
+
+//let
+//para declarar una variable
+//se necesita dos cosas
+//la palabra reservada y el nombre en donde quiero
+//almacenar el tipo de dato
+let nombre_de_pais 
+var nombrePresidente
+let edadDeMiPerro
+
+//variables asi no jamas joven nunca
+let @hola
+let 2hola
+let -nombre
+let x
+let uno
