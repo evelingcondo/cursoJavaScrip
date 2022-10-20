2bac4a23362215882131162595afcd4f99e86f68
cinthia <evelyng23cr@gmail.com>
2022-10-20 10:55:47 -0500

    trabajo de dos

---
+++ 3.operadores/ejercicios.js
@@ -0,0 +1,34 @@
+// 1.EJEMPLO DE OPERADORES DE ASIGNACION:
+// Asumimos las siguientes variables.
+//x=5
+//n=10
+// z=25
+// x=n 
+// la variable x contiene el valor 10 x=n=z
+//x=n (es decir 10 ) y z pisa el valor total remplazando por 25
+
+//2.EJEMPLO DE OPERADORES DE COMPARACION:
+1 == 1;                     // true
+"hola" == "hola";           // true
+
+//3.EJEMPLO DE OPERADORES ARITMETICOS:
+1 / 2; // 0.5
+1 / 2 == 1.0 / 2.0; // ESTO ES TRUE
+
+//4.EJEMPLO  DE OPERADORES BIT A BIT:
+var a = 4
+var b = 1
+document.write("A & B = " + (a & b) + '<br>')
+document.write("A | B = " + (a | b) + '<br>');
+document.write("~A = " + (~a) +'<br></br>'),
+
+//5.EJEMPLO DE OPERADORES DE CADENA:
+console.log( 'mi' + 'cadena'); //
+//la consola registra la cadena " mi cadena".
+
+//6.EJEMPLO DE OPERADORES RELACIONALES:
+var numero1 = 3:
+var numero2 = 5;
+resultado = numero1 > numero2; // resultado = false
+resultado = numero1 < numero2, // resultado = tue
+
---
+++ 3.operadores/index.js
@@ -0,0 +1,43 @@
+//ariasaurelio103@gmail.com
+
+//averiguar concepto de operador y tipos de operadores en java script
+//CONCEPTO 1:
+//Los operadores permiten manipular el valor de los variables
+//realizar operaciones matematicas cons sus valores y comparar diferentes variables
+//CONCEPTO 2:
+//Los peradores permiten realizar calculos complejos y tomar desiciones logicas
+//en funciones de comparaciones.
+//TIPOS DE JAVA SCRIPT:
+
+//1.operador de asignacion:
+//asigna un valor a su operando izquierdo basandose en el valor de su operando derecho.
+//el operador de asignacion simple es (=).
+
+//2.operadores de comparacion:
+//compara sus operandos y devuelve un valor logico en funcion de si la comparacion es verdadera o falsa.
+// los operandos pueden ser valores numericos,de cadena,logicos u objetos.
+//En caso que los 2 operandos no son del mismo tipo,javascript intenta convertirlos a un tipo
+//apropiado para la comparacion.
+
+//3.operadores aritmeticos:
+//toma valores numericos como operandos y devuelven un solo valor numerico.
+//javascript proporciona los operadores aritmeticos enumerados.
+
+//3.operadores de bit a bit:
+//trata asus operandos como un conjunto de 32 bits.
+//los operadores bit a bit realizan sus operaciones en representaciones binarias,pero devuelven
+// valores numericos standar de javascript.
+
+//4.operadores logicos:
+//se utilizan normalmente con valores booleanos,cuando lo son,devuelven un valor booleano.
+
+//5.peradores de cadena:
+//el operador de concatenacion,concatena 2 valores de cadena,devolviendo otra cadena que es 
+//la union de los 2 operandos decadena.
+
+//6.operadores relacionales:
+//compara sus operandos y devuelve un valor boolean basado en si la comparacion es verdadera.
+
+//CONCLUSION:
+//en conclusion todos los tipos de operadores
+// de javascript son muy utiles.
